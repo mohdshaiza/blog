c1b5befad3486640700f71b36bb16dab44aa43be
Mohd Shaiza <mohdshaiza@gmail.com>
2020-05-11 22:08:59 +0800

    Add layout page.

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Laravel 7 CRUD Application - ItSolutionStuff.com</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/boostrap.css">
+</head>
+
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+
+</html>
