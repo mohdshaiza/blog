a10bce387a9b9c8668d76cb5f7a5537c048bc5e1
Mohd Shaiza <mohdshaiza@gmail.com>
2020-05-12 00:04:44 +0800

    Fix update function.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -78,6 +78,13 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
+        $request->validate([
+            'name' => 'required',
+            'detail' => 'required',
+        ]);
+
+        $product->update($request->all());
+
         return redirect()->route('products.index')->with('success', 'Product updated successfully');
     }
 
--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -27,6 +27,12 @@
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
+                    <strong>Name:</strong>
+                    <input type="text" name="name" value="{{ $product->name }}" class="form-control" placeholder="Name">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
                     <strong>Detail:</strong>
                     <textarea class="form-control" style="height:150px" name="detail" placeholder="Detail">{{ $product->detail }}</textarea>
                 </div>
