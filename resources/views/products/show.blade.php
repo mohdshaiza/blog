a540f007fe44ccc7480f372618dbd37f5a9c8e1f
Mohd Shaiza <mohdshaiza@gmail.com>
2020-05-11 23:38:12 +0800

    Fix typo.

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -22,7 +22,7 @@
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Details:</strong>
-                {{ $product-detail }}
+                {{ $product->detail }}
             </div>
         </div>
     </div>
