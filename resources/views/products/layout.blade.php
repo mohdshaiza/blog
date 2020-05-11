277d03e8d98c3d8586a96be0c80fa48e413bf88c
Mohd Shaiza <mohdshaiza@gmail.com>
2020-05-11 23:24:19 +0800

    Fix css styles.

--- resources/views/products/layout.blade.php
+++ resources/views/products/layout.blade.php
@@ -3,7 +3,8 @@
 <html>
 <head>
     <title>Laravel 7 CRUD Application - ItSolutionStuff.com</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/boostrap.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/boostrap.css"> --}}
 </head>
 
 <body>
